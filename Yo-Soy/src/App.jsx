e6e1aa7e1a6b2daad908203c48098867d4720e50
alexnarvaeze <alexnarvaeze@gmail.com>
2024-05-01 23:33:04 -0400

    Created first nav bar

--- Yo-Soy/src/App.jsx
+++ Yo-Soy/src/App.jsx
@@ -5,8 +5,43 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const toggleMenu = () => {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  };
 
-  return <></>;
+  return (
+    <>
+      <nav id="desktop-nav">
+        <img
+          class="logo"
+          src="./src/assets/yo-soy-logo1.png"
+          alt="yo soy logo"
+        />
+        <div class="nav-color">
+          <ul class="nav-links">
+            <li>
+              <a href="#about">About Us</a>
+            </li>
+            <li>
+              <a href="#qualifications">Qualifications</a>
+            </li>
+            <li>
+              <a href="#services">Services</a>
+            </li>
+            <li>
+              <a href="#testimonials">Testimonials</a>
+            </li>
+            <li>
+              <a href="#contact">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default App;
