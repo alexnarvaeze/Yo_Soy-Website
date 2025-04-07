ec3994ea88a773e95417743b5021d61a3230222f
Matthew Larrosa <Matthewterra16@gmail.com>
2025-04-07 14:37:59 -0400

    refactored the hero section

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,33 +1,16 @@
-import React from "react";
 import "./HomePage.css";
+import heroImage from '../../assets/hero-eagle.jpg';
 
 function HomePage() {
   return (
-    <div className="sections-container">
-      {/* First Section */}
-      <div className="section home-page-container">
-        <div className="text-overlay">
-          <div className="home-main-container">
-            <h1 className="american-dream-header">
-              Here to help you achieve your American Dream!
-            </h1>
-            <p className="home-main-paragraph">
-              Expert Document Preparer and Notary specializing in Immigration
-              Services: Providing meticulous document preparation and
-              notarization services tailored to the complexities of immigration
-              paperwork. Ensuring accuracy, confidentiality, and efficiency in
-              facilitating smooth immigration processes for clients.
-            </p>
-          </div>
-        </div>
+    <div className='hero-container' style={{ backgroundImage: `url(${heroImage})` }}>
+      <div className='hero-text-container'>
+        <p className='hero-title'>Here to help you achieve your American Dream.</p>
+        <p className='hero-description'>Expert Document Preparer and Notary specializing in Immigration Services preparation and notarization tailored to the complexities of immigration paper work.</p>
       </div>
 
-      {/* Second Section */}
-      <div className="section">
-        <p>Thank you for trusting us with your immigration journey!</p>
-      </div>
     </div>
-  );
+  )
 }
 
-export default HomePage;
+export default HomePage
