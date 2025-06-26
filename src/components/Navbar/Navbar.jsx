0f15eb9fa17f19c4c62dd42d0db94bc838decf91
Narvaez Echeverria, Alex (FIU) <T0140999@miamidade.gov>
2025-06-26 17:36:07 -0400

    changed nav bar and fixed mobile responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,18 +3,20 @@ import "../Navbar/Navbar.css";
 import { useLanguage } from "../../context/LanguageContext";
 
 function Navbar() {
-  const { language } = useLanguage();
+  const { language, toggleLanguage } = useLanguage();
 
   const content = {
     en: {
       services: "services",
       aboutUs: "about us",
+      testimonials: "testimonials",
       qualifications: "qualifications",
       contactUs: "contact us",
     },
     es: {
       services: "servicios",
       aboutUs: "sobre nosotros",
+      testimonials: "testimonios",
       qualifications: "calificaciones",
       contactUs: "contáctenos",
     },
@@ -22,18 +24,31 @@ function Navbar() {
 
   return (
     <div className="navbar-container">
-      <div className="navbar-content">
-        <img
-          className="navbar-logo"
-          src={logo}
-          alt="Yo Soy Immigration Services"
-        />
-        <div className="navbar-links">
-          <a href="#services">{content[language].services}</a>|
-          <a href="#aboutus">{content[language].aboutUs}</a>|
-          <a href="#qualifications">{content[language].qualifications}</a>|
-          <a href="#contactus">{content[language].contactUs}</a>
-        </div>
+      <img
+        className="navbar-logo"
+        src={logo}
+        alt="Yo Soy Immigration Services"
+      />
+      <div className="navbar-links">
+        <a href="#services">{content[language].services}</a>|
+        <a href="#aboutus">{content[language].aboutUs}</a>|
+        <a href="#testimonials">{content[language].testimonials}</a>|
+        <a href="#qualifications">{content[language].qualifications}</a>|
+        <a href="#contactus">{content[language].contactUs}</a>
+      </div>
+      <div className="navbar-lang-switch">
+        <button
+          className={`lang-btn ${language === "en" ? "active" : ""}`}
+          onClick={() => toggleLanguage("en")}
+        >
+          EN
+        </button>
+        <button
+          className={`lang-btn ${language === "es" ? "active" : ""}`}
+          onClick={() => toggleLanguage("es")}
+        >
+          ES
+        </button>
       </div>
     </div>
   );
